6be5010b5833062243c36f65db62f2d188641e7d
Daniel Kovari <kovari.dani@gmail.com>
2024-01-12 15:30:55 +0100

    Fix game state backend

--- src/store.ts
+++ src/store.ts
@@ -1,24 +1,64 @@
 import { create } from "zustand";
 
-interface PlayerState {
+type Player = {
   id: number;
   health: number;
-  increment: () => void;
-  decrement: () => void;
+  counters: MagicCounter[];
+};
+
+type CounterType =
+  | "black"
+  | "white"
+  | "green"
+  | "blue"
+  | "red"
+  | "shock"
+  | "phyrexian"
+  | "star"
+  | "cloud"
+  | "shield"
+  | "sword"
+  | "mana";
+
+interface MagicCounter {
+  counterId: number;
+  type: CounterType;
+  value: number;
 }
 
 interface GameState {
-  players: PlayerState[];
+  players: Player[];
 
-  addPlayer: (player: PlayerState) => void;
-  deletePlayer: (player: PlayerState) => void;
+  addPlayer: (player: Player) => void;
+  deletePlayer: (player: Player) => void;
+  editPlayer: (playerId: number, values: Partial<Player>) => void;
 }
 
 export const useGameState = create<GameState>((set) => ({
   players: [],
-  addPlayer: (player) => set((state) => ({ players: [...state.players, player] })),
+  addPlayer: (player) =>
+    set((state) => ({ players: [...state.players, player] })),
   deletePlayer: (player) =>
     set((state) => ({
       players: state.players.filter((element) => element.id !== player.id),
     })),
+  editPlayer: (id, values) =>
+    set((state) => {
+      const player = state.players.find((player) => player.id === id);
+      if (!player) {
+        return state;
+      }
+      const newPlayer: Player = {
+        id: values.id ?? player.id,
+        health: values.health ?? player.health,
+        counters: values.counters ?? player.counters,
+      };
+      return {
+        ...state,
+        players: [
+          ...state.players.filter((player) => player.id !== id),
+          newPlayer,
+        ],
+      };
+    }),
 }));
